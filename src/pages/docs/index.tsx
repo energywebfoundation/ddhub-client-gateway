96a0772e6a3bb9743fd2d819fe903e2240d6e4fe
Adam Staveley <adam.staveley@energyweb.org>
2021-08-13 15:07:45 +0200

    add asyncapi support and ui

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -1,24 +1,45 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import Link from 'next/link'
+import Header from "components/Header/Header";
+import { Button, Container, makeStyles, Theme } from '@material-ui/core';
 
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-
-import spec from '../../../public/spec.json'
-
-export const getStaticProps: GetStaticProps = async (context) => {
-    return {
-        props: {
-            spec,
-        },
-    };
-};
-
-const ApiDoc = ({ spec }: InferGetStaticPropsType<typeof getStaticProps>) => {
+export default function Docs() {
+    const classes = useStyles()
     return (
-        <div style={{ backgroundColor: '#fff', padding: '2rem 0' }}>
-            <SwaggerUI spec={spec} />
+        <div>
+            <Header />
+            <Container className={classes.container}>
+                <div className={classes.button}>
+                    <Link href="/docs/rest" passHref={true}>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            fullWidth
+                        >
+                            REST API
+                        </Button>
+                    </Link>
+                </div>
+                <div className={classes.button}>
+                    <Link href="/docs/ws" passHref={true}>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            fullWidth
+                        >
+                            WebSocket API
+                        </Button>
+                    </Link>
+                </div>
+            </Container>
         </div>
-    );
-};
+    )
+}
 
-export default ApiDoc;
+const useStyles = makeStyles((theme: Theme) => ({
+    container: {
+        maxWidth: '500px'
+    },
+    button: {
+        margin: '2rem 0'
+    }
+}))
