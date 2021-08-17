0faf9c99429ae1360b991399a8bb62b50d8e2b09
vikaskumr <vikaskum660@gmail.com>
2021-08-17 17:28:32 +0530

    file upload functionality

--- src/components/UploadFile/Upload.tsx
+++ src/components/UploadFile/Upload.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 import { makeStyles } from '@material-ui/styles'
 import {
-  Typography,
-  Container,
-  Button,
-  Theme,
-  Grid,
-  Link,
+	Typography,
+	Container,
+	Button,
+	Theme,
+	Grid,
+	Link,
 } from '@material-ui/core';
 import { Info } from '@material-ui/icons'
 import { CustomInput } from 'components/CustomInput/CustomInput';
+import swal from 'sweetalert'
 
 
-export const Upload = () => {
-  const classes = useStyles()
+type UploadProps = {
+	onUpload: (file: File, channelName: string) => void
+}
+
+
+export const Upload = ({ onUpload }: UploadProps) => {
+	const classes = useStyles()
+
+	const [file, setFile] = useState<File>();
+	const [fileName, setFileName] = useState('');
+	const [channelName, setChannelName] = useState('')
+
+
+
+	const uploadToClient = (event) => {
+		if (event.target.files && event.target.files[0]) {
+			setFileName(event.target.files[0].name)
+			setFile(event.target.files[0]);
+		}
+	};
 
 	return (
 		<section className={classes.upload}>
 			<div className={classes.uploadHeader}>
-					<Info />
+				<Info />
 			</div>
 
 			<div className={classes.form}>
@@ -31,6 +50,17 @@ export const Upload = () => {
 							<CustomInput
 								placeholder='Fully Qualified Channel Name'
 								fullWidth
+								onChange={(event) => setChannelName(event.target.value)}
+							/>
+						</div>
+					</Grid>
+
+					<Grid item xs={12} sm={7} md={9}>
+						<div className={classes.formGroup}>
+							<Typography variant="caption">TOPIC NAME</Typography>
+							<CustomInput
+								placeholder='Fully Qualified Topic Name'
+								fullWidth
 							/>
 						</div>
 					</Grid>
@@ -40,9 +70,10 @@ export const Upload = () => {
 							<div className={classes.formGroup}>
 								<Typography variant="caption">FILE</Typography>
 								<CustomInput
-									placeholder='File'
+									placeholder={fileName ? fileName : 'No file chosen'}
 									fullWidth
 									disabled
+
 								/>
 							</div>
 						</Grid>
@@ -58,6 +89,7 @@ export const Upload = () => {
 								<input
 									type="file"
 									hidden
+									onChange={uploadToClient}
 								/>
 							</Button>
 						</Grid>
@@ -66,24 +98,32 @@ export const Upload = () => {
 								variant="outlined"
 								color="secondary"
 								fullWidth
+								onClick={() => {
+
+									if (!file) {
+										return swal('Error', 'No private key set', 'error')
+									}
+									onUpload(file, channelName)
+								}}
 							>
 								Upload
 							</Button>
 						</Grid>
+
 					</Grid>
 				</Grid>
 			</div>
 
 		</section>
-  )
+	)
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
-  upload: {
-    border: '1px solid #fff',
+	upload: {
+		border: '1px solid #fff',
 		padding: theme.spacing(6),
 		margin: theme.spacing(3, 1)
-  },
+	},
 	uploadHeader: {
 		textAlign: 'right',
 		color: '#fff'
@@ -103,15 +143,15 @@ const useStyles = makeStyles((theme: Theme) => ({
 		marginBottom: '2rem',
 
 		'& span': {
-				fontSize: '.8rem',
-				marginBottom: '.3rem'
+			fontSize: '.8rem',
+			marginBottom: '.3rem'
 		},
 		'& *': {
-				color: '#fff'
+			color: '#fff'
 		},
 		'& input': {
-				width: '100%'
-			}
+			width: '100%'
+		}
 	},
 	errorText: {
 		color: theme.palette.error.main
--- src/components/UploadFile/UploadContainer.tsx
+++ src/components/UploadFile/UploadContainer.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Upload } from './Upload';
+import axios from 'axios'
+import swal from '@sweetalert/with-react'
+import { useErrors } from 'hooks/useErrors'
 
-export const UploadContainer = () => {
+
+type UploadContainerProps = {
+	auth?: string
+}
+
+export const UploadContainer = ({ auth }: UploadContainerProps) => {
+	const errors = useErrors()
+	const [isLoading, setIsLoading] = useState(false)
+
+
+	const handleUpload = async (file: File, channelName: string) => {
+
+		setIsLoading(true)
+		const formData = new FormData();
+		formData.append("file", file);
+
+		try {
+			await axios.post(
+				`/api/v1/config/upload?channelname=${channelName}`,
+				formData,
+				auth
+					? { headers: { 'Authorization': `Bearer ${auth}`, 'content-type': 'multipart/form-data' } }
+					: undefined
+			)
+
+		} catch (err) {
+			swal('Error', errors(err.response.data.err), 'error');
+			setIsLoading(false);
+		}
+	}
+
 	return (
-		<Upload />
+		<Upload
+			onUpload={handleUpload}
+		/>
 	)
 }
