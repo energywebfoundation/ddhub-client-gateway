686f8290565b9ebc4f4eb01d8db0e4cfcf50731b
vikaskumr <vikas.kumar@energyweb.org>
2021-09-28 14:33:24 +0200

    get latest message API integrated in UI

--- src/components/DownloadFile/DownloadContainer.tsx
+++ src/components/DownloadFile/DownloadContainer.tsx
@@ -1,10 +1,43 @@
 import { Download } from './Download'
 import { Channel } from '../../utils'
+import { useState } from 'react'
+import axios from 'axios'
+import swal from '@sweetalert/with-react'
+
 
 type DownloadContainerProps = {
+  auth?: string
   channels: Channel[] | undefined
 }
 
-export const DownloadContainer = ({ channels }: DownloadContainerProps) => {
-  return <Download channels={channels} />
+export const DownloadContainer = ({ auth, channels }: DownloadContainerProps) => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleDownload = async (fqcn: string, amount: number, clientId?: string) => {
+    setIsLoading(true)
+    try {
+
+      let query = `fqcn=${fqcn}`
+      query += amount ? `&amount=${amount}` : ''
+      query += clientId ? `&clientId=${clientId}` : ''
+
+      const res = await axios.get(
+        `/api/v1/message?${query}`,
+        auth ? { headers: { Authorization: `Bearer ${auth}`, 'content-type': 'application/json' } } : undefined
+      )
+      const messages  = res.data
+      console.log('messages', messages)
+
+      swal(`Success`,`File downloaded Succesfully`, 'success')
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        swal('Error', err.response?.data?.err?.reason, 'error')
+      } else {
+        swal('Error', `Could not set identity: ${err}`, 'error')
+      }
+      setIsLoading(false)
+    }
+  }
+
+  return <Download channels={channels} onDownload={handleDownload} />
 }
