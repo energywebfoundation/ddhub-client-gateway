005f4bfc9f7395edbf70ca5357135727b0892792
vikaskumr <vikas.kumar@energyweb.org>
2021-10-21 14:33:58 +0200

    on basis of type file blob

--- src/components/DownloadFile/DownloadContainer.tsx
+++ src/components/DownloadFile/DownloadContainer.tsx
@@ -52,14 +52,20 @@ export const DownloadContainer = ({ auth, channels }: DownloadContainerProps) =>
       const fileType = typeof payload === 'object' ? 'json' : 'txt'
       const fileName = `${fqcn}_${new Date().getTime()}.${fileType}`
       const type = typeof payload === 'object' ? 'application/json' : 'application/text'
+      let blob
 
-      let payloadForFile = {
-        payload: res.data[0].payload,
-        messageId: res.data[0].id,
-        transactionId: res.data[0].transactionId
+      if (type === 'application/json') {
+        let payloadForFile = {
+          payload: res.data[0].payload,
+          messageId: res.data[0].id,
+          transactionId: res.data[0].transactionId
+        }
+        blob = new Blob([JSON.stringify(payloadForFile)], { type: type })
+      } else {
+        // eslint-disable-next-line max-len
+        blob = new Blob([`payload=${res.data[0].payload}`, `\ntransaction Id=${res.data[0].transactionId}`, `\nmesssage Id=${res.data[0].id}`], { type: type })
       }
 
-      const blob = new Blob([JSON.stringify(payloadForFile)], { type: type })
       const url = await window.URL.createObjectURL(blob)
       const tempLink = document.createElement('a')
       tempLink.href = url
