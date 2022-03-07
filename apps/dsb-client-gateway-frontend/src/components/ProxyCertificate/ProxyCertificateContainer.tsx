801e540a1aa3dfe14a43db2a7cf16710af6ff736
Krzysztof Szostak <christopher.szostak@energyweb.org>
2022-03-07 13:45:06 +0100

    feat: connect certificate

--- apps/dsb-client-gateway-frontend/src/components/ProxyCertificate/ProxyCertificateContainer.tsx
+++ apps/dsb-client-gateway-frontend/src/components/ProxyCertificate/ProxyCertificateContainer.tsx
@@ -1,47 +1,47 @@
-import { useState } from 'react'
-import swal from '@sweetalert/with-react'
-import axios from 'axios'
-import { ProxyCertificate } from './ProxyCertificate'
-import { CertificateFiles } from '../../utils'
+import { useState } from 'react';
+import swal from '@sweetalert/with-react';
+import axios from 'axios';
+import { ProxyCertificate } from './ProxyCertificate';
+import { CertificateFiles } from '../../utils';
 
 type ProxyCertificateContainerProps = {
-  certificate?: CertificateFiles
-  auth?: string
-}
+  certificate?: CertificateFiles;
+  auth?: string;
+};
 
-export const ProxyCertificateContainer = ({ certificate, auth }: ProxyCertificateContainerProps) => {
-  const [isLoading, setIsLoading] = useState(false)
+export const ProxyCertificateContainer = ({
+  certificate,
+  auth,
+}: ProxyCertificateContainerProps) => {
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (cert: File, key: File, ca?: File) => {
-    setIsLoading(true)
-    const formData = new FormData()
-    formData.append('cert', cert)
-    formData.append('key', key)
+    setIsLoading(true);
+    const formData = new FormData();
+    formData.append('certificate', cert);
+    formData.append('privateKey', key);
+
     if (ca) {
-      formData.append('ca', ca)
+      formData.append('caCertificate', ca);
     }
 
     try {
-      const res = await axios.post(
-        `/api/v1/config/certificate`,
-        formData,
-        {
-          headers: {
-            Authorization: auth ? `Bearer ${auth}` : undefined,
-            'content-type': 'multipart/form-data'
-          }
-        }
-      )
-      swal('Success', 'Certificate saved', 'success')
+      const res = await axios.post(`/v1/certificate`, formData, {
+        headers: {
+          Authorization: auth ? `Bearer ${auth}` : undefined,
+          'content-type': 'multipart/form-data',
+        },
+      });
+      swal('Success', 'Certificate saved', 'success');
     } catch (err) {
       if (axios.isAxiosError(err)) {
-        swal('Error', err.response?.data?.err?.reason, 'error')
+        swal('Error', err.response?.data?.err?.reason, 'error');
       } else {
-        swal('Error', `Could not set identity: ${err}`, 'error')
+        swal('Error', `Could not set identity: ${err}`, 'error');
       }
     }
-    setIsLoading(false)
-  }
+    setIsLoading(false);
+  };
 
   return (
     <ProxyCertificate
@@ -49,5 +49,5 @@ export const ProxyCertificateContainer = ({ certificate, auth }: ProxyCertificat
       isLoading={isLoading}
       onSubmit={handleSubmit}
     />
-  )
-}
+  );
+};
