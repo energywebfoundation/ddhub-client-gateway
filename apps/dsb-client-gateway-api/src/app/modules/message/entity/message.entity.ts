c110797b24c392177f94da23b049d14ff5e0541b
vikaskumr <vikas.kumar@energyweb.org>
2022-03-28 18:28:04 +0530

    fix: post message is review changes

--- apps/dsb-client-gateway-api/src/app/modules/message/entity/message.entity.ts
+++ apps/dsb-client-gateway-api/src/app/modules/message/entity/message.entity.ts
@@ -1,37 +1,37 @@
 export interface SendMessageSuccessResponse {
-    did: string
-    messageId: string
-    statusCode: number
-    err: {
-        code: string
-        reason: string
-        additionalInformation: object
-    }
+  did: string;
+  messageId: string;
+  statusCode: number;
+  err: {
+    code: string;
+    reason: string;
+    additionalInformation: object;
+  };
 }
 export interface SendMessageFailedResponse {
-    did: string
-    messageId: string
-    statusCode: number
-    err: {
-        code: string
-        reason: string
-        additionalInformation: object
-    }
+  did: string;
+  messageId: string;
+  statusCode: number;
+  err: {
+    code: string;
+    reason: string;
+    additionalInformation: object;
+  };
 }
 
 export interface recipients {
-    total: number
-    sent: number
-    failed: number
+  total: number;
+  sent: number;
+  failed: number;
 }
 
-export interface SendInetrnalMessageResponse {
-    id: string;
+export interface SendInternalMessageResponse {
+  id: string;
 }
 export interface SendMessageResponse {
-    clientGatewayMessageId: string
-    recipients: recipients
-    did: string
-    success: SendMessageSuccessResponse[]
-    failed: SendMessageFailedResponse[]
+  clientGatewayMessageId: string;
+  recipients: recipients;
+  did: string;
+  success: SendMessageSuccessResponse[];
+  failed: SendMessageFailedResponse[];
 }
--- apps/dsb-client-gateway-api/src/app/modules/message/message.interface.ts
+++ apps/dsb-client-gateway-api/src/app/modules/message/message.interface.ts
@@ -1,32 +1,32 @@
 export interface SendMessageSuccessResponse {
-    did: string
-    messageId: string
-    statusCode: number
+  did: string;
+  messageId: string;
+  statusCode: number;
 }
 export interface SendMessageFailedResponse {
-    did: string
-    messageId: string
-    statusCode: number
-    err: {
-        code: string
-        reason: string
-        additionalInformation: object
-    }
+  did: string;
+  messageId: string;
+  statusCode: number;
+  err: {
+    code: string;
+    reason: string;
+    additionalInformation: object;
+  };
 }
 
-export interface SendInetrnalMessageResponse {
-    id: string;
+export interface SendInternalMessageResponse {
+  id: string;
 }
 
 export interface recipients {
-    total: number
-    sent: number
-    failed: number
+  total: number;
+  sent: number;
+  failed: number;
 }
 export interface SendMessageResponse {
-    clientGatewayMessageId: string
-    did: string
-    recipients: recipients
-    success: SendMessageSuccessResponse[]
-    failed: SendMessageFailedResponse[]
+  clientGatewayMessageId: string;
+  did: string;
+  recipients: recipients;
+  success: SendMessageSuccessResponse[];
+  failed: SendMessageFailedResponse[];
 }
