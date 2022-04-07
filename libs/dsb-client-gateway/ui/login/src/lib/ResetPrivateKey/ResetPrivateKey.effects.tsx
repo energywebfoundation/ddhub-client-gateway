4677262f53780a4520364fbb0d1fa2aed89dffd4
dawidgil <dawid.gil@j-labs.pl>
2022-04-07 21:55:42 +0200

    refactor: move logic to effect for reset private key component

---
+++ libs/dsb-client-gateway/ui/login/src/lib/ResetPrivateKey/ResetPrivateKey.effects.tsx
@@ -0,0 +1,27 @@
+import { useContext } from 'react';
+import { UserDataContext } from '@dsb-client-gateway/ui/login';
+import Swal from 'sweetalert2';
+import { theme } from '@dsb-client-gateway/ui/utils';
+import { AccountStatusEnum } from '../check-account-status/check-account-status';
+
+export const useResetPrivateKey = () => {
+  const {userData, setUserData} = useContext(UserDataContext);
+
+  const resetPrivateKeyHandler = () => {
+    Swal.fire( {
+      icon: 'warning',
+      iconColor: theme.palette.warning.main,
+      title: 'Reset private key',
+      text: 'Please confirm to reset private key',
+      showCancelButton: true,
+      showConfirmButton: true,
+      confirmButtonText: 'Confirm'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setUserData({...userData, accountStatus: AccountStatusEnum.NotSetPrivateKey});
+      }
+    })
+  };
+
+  return {resetPrivateKeyHandler}
+}
