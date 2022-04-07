96c6d6795abacb074ef60e61dd41818d42204190
dawidgil <dawid.gil@j-labs.pl>
2022-04-07 21:53:00 +0200

    refactor: move logic to effect for login status component

---
+++ libs/dsb-client-gateway/ui/login/src/lib/LoginStatus/RequestingEnrolment/LoginStatus.effects.tsx
@@ -0,0 +1,40 @@
+import { usePrivateKey } from '../../Login.effects';
+import { AccountStatusEnum } from '../../check-account-status/check-account-status';
+import LoginForm from '../../LoginForm/LoginForm';
+import InsufficientFund from '../InsufficientFund/InsufficientFund';
+import { RoleStatus } from '@dsb-client-gateway/dsb-client-gateway/identity/models';
+import EnrolForRoleContainer from '../EnrolForRoleContainer/EnrolForRoleContainer';
+import RequestingEnrolment from './RequestingEnrolment';
+import AwaitingSyncing from '../AwaitingSyncing/AwaitingSyncing';
+import ResetPrivateKey from '../../ResetPrivateKey/ResetPrivateKey';
+
+export const useLoginStatus = () => {
+  const {isLoading, submit, status} = usePrivateKey();
+
+  const privateKeyHandler = (privateKey: string) => {
+    submit(privateKey);
+  };
+
+  const statusFactory = () => {
+    switch (status) {
+      case AccountStatusEnum.NotSetPrivateKey:
+        return <LoginForm onPrivateKeySubmit={privateKeyHandler} />;
+      case AccountStatusEnum.InsufficientFund:
+        return <InsufficientFund/>;
+      case RoleStatus.NOT_ENROLLED:
+        return <EnrolForRoleContainer/>;
+      case RoleStatus.AWAITING_APPROVAL:
+        return <RequestingEnrolment/>;
+      case RoleStatus.APPROVED:
+        return <AwaitingSyncing/>;
+      default:
+        return (
+          <>
+            <div>Ops! Something went wrong!</div>
+            <ResetPrivateKey/>
+          </>);
+    }
+  };
+
+  return {statusFactory, isLoading}
+}
