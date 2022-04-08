b2722805a4a1cde2ce09635bf9e707086b87a09d
driamend <driamend@gmail.com>
2022-04-08 12:06:15 +0300

    feat(frontend): set identity on success

--- libs/dsb-client-gateway/ui/login/src/lib/UserDataContext.ts
+++ libs/dsb-client-gateway/ui/login/src/lib/UserDataContext.ts
@@ -1,16 +1,12 @@
 import { createContext, useMemo, useState } from 'react';
-import { BalanceState, RoleState, RoleStatus } from '@dsb-client-gateway/dsb-client-gateway/identity/models';
+import { RoleStatus } from '@dsb-client-gateway/dsb-client-gateway/identity/models';
 import { AccountStatusEnum } from './check-account-status/check-account-status';
 export interface UserDataContext {
-  publicKey: string;
-  address: string;
-  balance: BalanceState;
-  did: string | null;
   loggedIn: boolean;
   accountStatus: AccountStatusEnum | RoleStatus;
 }
 
-const initialData = {publicKey: '', address: '', balance: BalanceState.NONE, did: '', loggedIn: false, accountStatus: AccountStatusEnum.NotSetPrivateKey}
+const initialData = {loggedIn: false, accountStatus: AccountStatusEnum.NotSetPrivateKey}
 export const UserDataContext = createContext<{userData: UserDataContext; setUserData: (data: UserDataContext) => void}>({
   userData: initialData,
   // eslint-disable-next-line @typescript-eslint/no-empty-function
