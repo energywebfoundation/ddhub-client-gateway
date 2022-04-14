b3390224c3e721fb255cf6060330afbdba4fbe5f
driamend <driamend@gmail.com>
2022-04-14 14:11:20 +0300

    chore(ui): add env to enable mock

--- apps/dsb-client-gateway-frontend/src/services/mock.service.ts
+++ apps/dsb-client-gateway-frontend/src/services/mock.service.ts
@@ -1,29 +1,24 @@
 import { createServer } from 'miragejs';
 import {
   getApplicationsControllerGetApplicationsMock,
-  getIdentityControllerGetMock,
+  getIdentityControllerGetMock
 } from '@dsb-client-gateway/dsb-client-gateway-api-client';
 
-export function makeServer({
-  environment = 'development',
-  serverBaseUrl = 'http://localhost:3333',
-}) {
-  if (environment === 'production' || serverBaseUrl !== 'http://localhost:3333')
-    return;
+export function makeServer({ environment = 'development' }) {
   return createServer({
     environment,
     routes() {
       this.passthrough('/_next/static/development/_devPagesManifest.json');
       this.passthrough('/_next/static/development/_devMiddlewareManifest.json');
       this.namespace = 'api/v2';
-      this.urlPrefix = serverBaseUrl;
+      this.urlPrefix = 'http://localhost:3333';
 
       this.get('/applications', () => {
-        return getApplicationsControllerGetApplicationsMock();
+        return getApplicationsControllerGetApplicationsMock()
       });
 
       this.get('/identity', () => {
-        return getIdentityControllerGetMock();
+        return getIdentityControllerGetMock()
       });
     },
   });
