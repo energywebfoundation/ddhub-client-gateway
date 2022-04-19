938a683b50a379c93058fdf16ee8047da04d50f9
dawidgil <dawid.gil@j-labs.pl>
2022-04-19 23:25:27 +0200

    feat: generate library for channels

---
+++ libs/dsb-client-gateway/ui/channels/src/lib/dsb-client-gateway-ui-channels.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable-next-line */
+export interface DsbClientGatewayUiChannelsProps {}
+
+export function DsbClientGatewayUiChannels(
+  props: DsbClientGatewayUiChannelsProps
+) {
+  return (
+    <div>
+      <h1>Welcome to DsbClientGatewayUiChannels!</h1>
+    </div>
+  );
+}
+
+export default DsbClientGatewayUiChannels;
