65fc6006992b148b92d3468fdc0c5a3c204fb6bc
driamend <driamend@gmail.com>
2022-04-21 17:20:54 +0300

    feat(ui): add placeholder for app image

--- libs/dsb-client-gateway-api-client/src/lib/mocks/index.ts
+++ libs/dsb-client-gateway-api-client/src/lib/mocks/index.ts
@@ -20,7 +20,7 @@ export const getIdentityControllerGetMock = () => ({
 export const getApplicationsControllerGetApplicationsMock = () => [
   {
     appName: faker.word.noun(),
-    logoUrl: faker.image.abstract(),
+    logoUrl: '',
     websiteUrl: faker.random.word(),
     description: faker.random.word(),
     namespace: 'ddhub.apps.energyweb.iam.ewc',
