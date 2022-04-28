a75b410ffc4cac366c0b97f1b7124e7b2c7ac702
driamend <driamend@gmail.com>
2022-04-28 11:21:40 +0300

    feat(ui): add backdrop

---
+++ libs/dsb-client-gateway/ui/core/src/components/common/Backdrop/Backdrop.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import {
+  Backdrop as MaterialBackdrop,
+  CircularProgress,
+  Box,
+  Typography,
+} from '@mui/material';
+import { useStyles } from './Backdrop.styles';
+import { alpha } from '@mui/material/styles';
+
+export const Backdrop: FC = () => {
+  const { classes, theme } = useStyles();
+  return (
+    <MaterialBackdrop open={true} className={classes.root}>
+      <Box className={classes.wrapper}>
+        <svg style={{ position: 'absolute' }}>
+          <defs>
+            <linearGradient id="gradient" x1="0%" y1="100%" x2="100%" y2="100%">
+              <stop offset="5%" stopColor={theme.palette.primary.main} />
+              <stop
+                offset="95%"
+                stopColor={alpha(theme.palette.primary.main, 0.35)}
+              />
+            </linearGradient>
+          </defs>
+        </svg>
+        <CircularProgress
+          size={75}
+          thickness={2}
+          disableShrink
+          sx={{ 'svg circle': { stroke: 'url(#gradient)' } }}
+        />
+        <Box className={classes.logoWrapper}>
+          <img src="/ew-logo.svg" alt="logo" className={classes.logo} />
+        </Box>
+      </Box>
+      <Box>
+        <Typography variant="body2" className={classes.text}>
+          Loading...
+        </Typography>
+      </Box>
+    </MaterialBackdrop>
+  );
+};
