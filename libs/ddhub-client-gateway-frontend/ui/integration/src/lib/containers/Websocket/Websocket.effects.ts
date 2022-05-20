7a4d27308480cc5fd6e2de048536e8339e5e0c13
driamend <driamend@gmail.com>
2022-05-20 16:42:43 +0300

    feat(ddhub-client-gateway-ui-integration): fix react component

--- libs/ddhub-client-gateway-frontend/ui/integration/src/lib/containers/Websocket/Websocket.effects.ts
+++ libs/ddhub-client-gateway-frontend/ui/integration/src/lib/containers/Websocket/Websocket.effects.ts
@@ -1,15 +1,7 @@
-import { useEffect } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
 export const useWebsocketEffects = () => {
-  useEffect(
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    () => import('@asyncapi/web-component/lib/asyncapi-web-component'),
-    []
-  );
-
   const result = useQuery(
     'ws',
     async () => (await axios.get('/ws.yaml', { baseURL: '' }))?.data
