d4991454a4016761d8febc159296e322358a3243
driamend <driamend@gmail.com>
2022-05-23 12:31:48 +0300

    feat(dsb-client-gateway-frontend): add gateway settings page

--- apps/dsb-client-gateway-frontend/src/pages/gateway-settings/index.tsx
+++ apps/dsb-client-gateway-frontend/src/pages/gateway-settings/index.tsx
@@ -1,34 +1,5 @@
-import { makeStyles } from 'tss-react/mui';
-import { Container, Grid } from '@mui/material';
+import { Settings } from '@ddhub-client-gateway-frontend/ui/gateway-settings';
 
 export default function Index() {
-  const {classes} = useStyles();
-  return (
-    <div>
-      <main>
-        <Container maxWidth="lg">
-            <section className={classes.main}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} md={6}>
-                  {/*<GatewayIdentityContainer*/}
-                  {/*  identity={state.ok?.identity}*/}
-                  {/*  enrolment={state.ok?.enrolment}*/}
-                  {/*  auth={auth.some}*/}
-                  {/*/>*/}
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  {/*<ProxyCertificateContainer certificate={state.ok?.certificate} auth={auth.some}/>*/}
-                </Grid>
-              </Grid>
-            </section>
-        </Container>
-      </main>
-    </div>
-  );
+  return <Settings />;
 }
-const useStyles = makeStyles()(theme => ({
-  main: {
-    padding: '0 1rem',
-    marginTop: '2rem'
-  },
-}));
