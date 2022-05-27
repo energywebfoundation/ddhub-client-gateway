50c727a4fbd422442b00fec0adbee9afcf006423
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2022-05-27 12:28:04 +0800

    feat(ddhub-client-gateway-ui-api-hooks): topics server side pagination

--- libs/ddhub-client-gateway-frontend/ui/api-hooks/src/lib/topics/getTopics.ts
+++ libs/ddhub-client-gateway-frontend/ui/api-hooks/src/lib/topics/getTopics.ts
@@ -1,13 +1,19 @@
 import { keyBy } from 'lodash';
+import { useQueryClient } from 'react-query';
 import {
   GetTopicDto,
   PaginatedResponse,
   useTopicsControllerGetTopics,
+  TopicsControllerGetTopicsParams,
+  getTopicsControllerGetTopicsQueryKey,
+  topicsControllerGetTopics
 } from '@dsb-client-gateway/dsb-client-gateway-api-client';
+import {useState} from "react";
 
-export const useTopics = (owner: string) => {
+export const useTopics = ({ limit = 0, page = 1, owner }: TopicsControllerGetTopicsParams) => {
+  const queryClient = useQueryClient();
   const { data, isLoading, isSuccess, isError } = useTopicsControllerGetTopics(
-    { owner },
+    { owner, limit, page },
     {
       query: {
         enabled: !!owner,
@@ -16,14 +22,37 @@ export const useTopics = (owner: string) => {
   );
 
   const paginated = data ?? ({} as PaginatedResponse);
-  const topics = paginated.records ?? ([] as GetTopicDto[]);
+  const topicsInitialState = paginated.records ?? ([] as GetTopicDto[]);
+  const [topics, setTopics] = useState(topicsInitialState);
   const topicsById = keyBy(topics, 'id');
   const topicsFetched = isSuccess && data !== undefined && !isError;
+  const paginationInitialState = {
+    limit: paginated.limit,
+    count: paginated.count,
+    page: paginated.page,
+  };
+  const [pagination, setPagination] = useState(paginationInitialState);
+
+  const getTopics = async ({ limit = 6, owner, page }: TopicsControllerGetTopicsParams) => {
+    const topicData = await queryClient.fetchQuery({
+      queryKey: getTopicsControllerGetTopicsQueryKey({ limit, owner, page }),
+      queryFn: () => topicsControllerGetTopics({ limit, page, owner })
+    });
+
+    setTopics(topicData.records);
+    setPagination({
+      limit: topicData.limit,
+      page: topicData.page,
+      count: topicData.count,
+    });
+  };
 
   return {
     topics,
     topicsById,
     isLoading,
     topicsFetched,
+    getTopics,
+    pagination,
   };
 };
