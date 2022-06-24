4515759fbc05a346493cf7f7080318e9dddca9a6
Krzysztof Szostak <christopher.szostak@energyweb.org>
2022-06-24 08:53:39 +0200

    chore(dsb-client-gateway-api): introduce semantic releases

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/github',
+      {
+        assets: ['dist/**'],
+      },
+    ],
+    '@semantic-release/git',
+  ],
+  publish: false,
+  preset: 'angular',
+};
