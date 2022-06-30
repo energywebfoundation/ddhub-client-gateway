e83156651e49c45c51581a8c981b14ae9ad6d6ce
dawidgil <dawid.gil@j-labs.pl>
2022-06-30 09:19:22 +0200

    fix(ddhub-client-gateway-ui-login): clear cache when identity changes

--- libs/ddhub-client-gateway-frontend/ui/gateway-settings/src/lib/containers/GatewayIdentity/GatewayIdentity.effects.ts
+++ libs/ddhub-client-gateway-frontend/ui/gateway-settings/src/lib/containers/GatewayIdentity/GatewayIdentity.effects.ts
@@ -3,10 +3,12 @@ import { IMPORT_PRIVATE_KEY } from '@ddhub-client-gateway/identity/models';
 import { useCustomAlert } from '@ddhub-client-gateway-frontend/ui/core';
 import { useIdentity } from '@ddhub-client-gateway-frontend/ui/api-hooks';
 import { routerConst } from '@ddhub-client-gateway-frontend/ui/utils';
+import { useQueryClient } from 'react-query';
 
 export const useGatewayIdentityEffects = () => {
   const router = useRouter();
   const { identity } = useIdentity();
+  const queryClient = useQueryClient();
   const Swal = useCustomAlert();
 
   const namespace =
@@ -19,10 +21,12 @@ export const useGatewayIdentityEffects = () => {
     });
 
     if (result.isConfirmed) {
-      router.push({
-        pathname: routerConst.InitialPage,
-        query: { privateKey: IMPORT_PRIVATE_KEY },
-      });
+      router
+        .push({
+          pathname: routerConst.InitialPage,
+          query: { privateKey: IMPORT_PRIVATE_KEY },
+        })
+        .then(() => queryClient.clear());
     }
   };
 
