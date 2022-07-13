25a99aee334631a1f9d9d5065a5ae8120c89e1e3
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2022-07-13 11:10:46 +0800

    fix(ddhub-client-gateway-ui-channels): fix topics filter after select

--- libs/ddhub-client-gateway-frontend/ui/channels/src/lib/containers/modals/Create/Create.effects.ts
+++ libs/ddhub-client-gateway-frontend/ui/channels/src/lib/containers/modals/Create/Create.effects.ts
@@ -14,6 +14,7 @@ import { TActionButtonsProps } from "./ActionButtons/ActionButtons";
 import { ICreateChannel } from "../models/create-channel.interface";
 import { ChannelType } from "../../../models/channel-type.enum";
 import { ConnectionType } from "./Details/models/connection-type.enum";
+import { pick } from 'lodash';
 
 type TGetActionButtonsProps = TActionButtonsProps['nextClickButtonProps'] & {
   canGoBack: boolean;
@@ -93,12 +94,14 @@ export const useCreateChannelEffects = () => {
   };
 
   const setTopics = (data: Topic[]) => {
+    const topicsData = data.map(topic => pick(topic, ['owner', 'topicName']));
+
     setActiveStep(activeStep + 1);
     setChannelValues({
       ...channelValues,
       conditions: {
         ...channelValues.conditions,
-        topics: data,
+        topics: topicsData,
       },
     });
   };
