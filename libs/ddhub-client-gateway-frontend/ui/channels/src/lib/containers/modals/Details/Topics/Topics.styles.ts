b0d0714950fa6e5ac311477f43ea4b889522f1db
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2022-07-26 12:34:44 +0800

    feat(ddhub-client-gateway-ui-channels): increase topic maxheight

--- libs/ddhub-client-gateway-frontend/ui/channels/src/lib/containers/modals/Details/Topics/Topics.styles.ts
+++ libs/ddhub-client-gateway-frontend/ui/channels/src/lib/containers/modals/Details/Topics/Topics.styles.ts
@@ -26,7 +26,7 @@ export const useStyles = makeStyles()((theme) => ({
     color: theme.palette.grey[300],
   },
   topicsList: {
-    maxHeight: 132,
+    maxHeight: 500,
     marginTop: 15,
     overflow: 'auto',
     '&::-webkit-scrollbar': {
