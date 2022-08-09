e2f9e8c2a92eca1841e3dba31d20b6206ce52e00
azam.ismail <azam.ismail@energyweb.org>
2022-08-09 19:02:09 +0800

    feat(dsb-client-gateway-api): enable heartbeat for ws server mode

--- apps/dsb-client-gateway-api/src/app/modules/message/service/dsb-message-pooling.service.ts
+++ apps/dsb-client-gateway-api/src/app/modules/message/service/dsb-message-pooling.service.ts
@@ -14,6 +14,7 @@ import { WsClientService } from './ws-client.service';
 
 enum SCHEDULER_HANDLERS {
   MESSAGES = 'ws-messages',
+  MESSAGES_HEARTBEAT = 'ws-messages-heartbeat',
 }
 
 @Injectable()
@@ -44,8 +45,37 @@ export class DsbMessagePoolingService implements OnModuleInit {
     const timeout = setTimeout(callback, this.configService.get<number>('WEBSOCKET_POOLING_TIMEOUT', 5000));
     this.schedulerRegistry.addTimeout(SCHEDULER_HANDLERS.MESSAGES, timeout);
 
+
+    const callbackHeartbeat = async () => {
+      await this.handleIntervalHeartbeat();
+    };
+    const timeoutHeartbeat = setTimeout(callbackHeartbeat, this.configService.get<number>('WEBSOCKET_HEARTBEAT_TIMEOUT', 30000));
+    this.schedulerRegistry.addTimeout(SCHEDULER_HANDLERS.MESSAGES_HEARTBEAT, timeoutHeartbeat);
+
     this.logger.log('Enabling websockets');
 
+  }
+
+  @Span('ws_pool_messages_heartbeat')
+  public async handleIntervalHeartbeat(): Promise<void> {
+    const callbackHeartbeat = async () => {
+      await this.handleIntervalHeartbeat();
+    };
+
+    this.schedulerRegistry.deleteTimeout(SCHEDULER_HANDLERS.MESSAGES_HEARTBEAT);
+
+    if (this.websocketMode === WebSocketImplementation.SERVER && this.gateway.server.clients.size > 0) {
+      await Promise.allSettled(
+        Array.from(this.gateway.server.clients.values()).map((client: any) => {
+          const _clientId = new URLSearchParams(client.request?.url.split("?")[1]).get("clientId");
+          client.send(JSON.stringify({ clientId: `${_clientId}`, type: 'Heartbeat', messages: `Heartbeat trigger. Every ${this.configService.get<number>('WEBSOCKET_HEARTBEAT_TIMEOUT', 30000) / 1000} seconds` }));
+        })
+      );
+      const timeoutHeartbeat = setTimeout(callbackHeartbeat, this.configService.get<number>('WEBSOCKET_HEARTBEAT_TIMEOUT', 30000));
+      this.schedulerRegistry.addTimeout(SCHEDULER_HANDLERS.MESSAGES_HEARTBEAT, timeoutHeartbeat);
+      this.logger.log(`${this.gateway.server.clients.size} client connected. Heartbeat trigger. Every ${this.configService.get<number>('WEBSOCKET_HEARTBEAT_TIMEOUT', 30000)}`);
+      return;
+    }
   }
 
   @Span('ws_pool_messages')
