c967dce0c1d303bfad72c3ab8197b1349c6c631f
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2022-08-29 16:34:58 +0800

    feat(ddhub-client-gateway-ui-messaging): new file upload ui

--- libs/ddhub-client-gateway-frontend/ui/messaging/src/lib/containers/UploadForm/UploadForm.styles.ts
+++ libs/ddhub-client-gateway-frontend/ui/messaging/src/lib/containers/UploadForm/UploadForm.styles.ts
@@ -1,16 +1,63 @@
 import { makeStyles } from 'tss-react/mui';
+import { alpha } from '@mui/material/styles';
 
 export const useStyles = makeStyles()((theme) => ({
   wrapper: {
-    border: `1px solid ${theme.palette.grey[500]}`,
+    border: '1px solid #384151',
     borderRadius: 5,
     minHeight: 189,
-    padding: '23px 21px 26px 21px',
+    padding: '21px 21px 26px 21px',
+    background: '#283046',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  uploadWrapper: {
+    border: '1px dashed #848484',
+    padding: '65px 10px 55px 10px',
+    background: '#20273D',
+    borderRadius: 5,
+  },
+  uploadWrapperDisabled: {
+    borderColor: '#676D7D',
+    background: '#262D43',
+    cursor: 'no-drop',
+  },
+  uploadWrapperHover: {
+    '&:hover': {
+      borderColor: theme.palette.primary.main,
+      boxShadow: `2px 2px 12px ${alpha(theme.palette.primary.main, 0.27)}`,
+      cursor: 'pointer',
+    },
+  },
+  uploadedWrapperHover: {
+    '&:hover': {
+      background: '#262D43',
+      cursor: 'pointer',
+      '#replace-message': {
+        display: 'block',
+      },
+      '#upload-message': {
+        display: 'none',
+      },
+      '#fileUploadPartial': {
+        fill: '#676D7D !important',
+      },
+    },
+  },
+  title: {
+    fontSize: 16,
+    lineHeight: '19.2px',
+    fontWeight: 500,
+    color: theme.palette.common.white,
+  },
+  titleDisabled: {
+    color: '#676D7D',
   },
   label: {
-    fontSize: 18,
-    lineHeight: '22px',
-    fontWeight: 500,
-    color: theme.palette.text.primary,
-  }
+    paddingTop: 4,
+    fontSize: 12,
+    lineHeight: '14.16px',
+    fontWeight: 405,
+    color: '#D0D2D6',
+  },
 }));
--- libs/ddhub-client-gateway-frontend/ui/messaging/src/lib/containers/UploadForm/UploadForm.tsx
+++ libs/ddhub-client-gateway-frontend/ui/messaging/src/lib/containers/UploadForm/UploadForm.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
-import { Box, BoxProps, Grid, Typography } from '@mui/material';
+import { Box, BoxProps, Typography } from '@mui/material';
 import { UploadInput } from '@ddhub-client-gateway-frontend/ui/core';
 import { TFileType } from './UploadForm.types';
 import { useUploadFormEffects } from './UploadForm.effects';
 import { useStyles } from './UploadForm.styles';
+import clsx from 'clsx';
 
 export interface UploadFormProps {
   acceptedFiles: File[];
@@ -29,37 +30,40 @@ export const UploadForm: FC<UploadFormProps> = ({
 
   return (
     <Box className={classes.wrapper} {...wrapperProps}>
-      <Grid container>
-        <Grid item sx={{ minWidth: 183 }}>
-          <Typography className={classes.label} variant="body1">
-            Upload file
+      <Box
+        display="flex"
+        flexDirection="column"
+        mb={2.5}
+        sx={{ minHeight: '44px' }}
+      >
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Typography className={clsx(classes.title, {
+            [classes.titleDisabled]: !acceptedFileType,
+          })} variant="body1">
+            Upload {acceptedFileType ? acceptedFileType.toUpperCase() : 'file'}
           </Typography>
-        </Grid>
-        <Grid item>
-          <Box>
-            <Box
-              display="flex"
-              flexDirection="column"
-              mt={3.8}
-              mb={4.1}
-              sx={{ minHeight: '44px' }}
-            >
-              {acceptedFileType && (
-                <Box display="flex" flexDirection="column">
-                  <Typography variant="body1">File:</Typography>
-                  <Typography variant="body2">{fileSizeInfo}</Typography>
-                </Box>
-              )}
-            </Box>
-            <UploadInput
-              acceptedFileType={acceptedFileType}
-              value={fileTextValue}
-              onChange={onFileChange}
-              maxSize={maxFileSize}
-            />
-          </Box>
-        </Grid>
-      </Grid>
+          {acceptedFileType && (
+            <Typography className={classes.label} variant="body2">{fileSizeInfo}</Typography>
+          )}
+        </Box>
+      </Box>
+      <Box>
+        <UploadInput
+          uploadBox={true}
+          acceptedFileType={acceptedFileType}
+          value={fileTextValue}
+          onChange={onFileChange}
+          maxSize={maxFileSize}
+          isUploadDisabled={!acceptedFileType}
+          wrapperProps={{
+            className: clsx(classes.uploadWrapper, {
+            [classes.uploadWrapperDisabled]: !acceptedFileType,
+            [classes.uploadWrapperHover]: acceptedFileType && !fileTextValue,
+            [classes.uploadedWrapperHover]: acceptedFileType && fileTextValue,
+            }),
+          }}
+        />
+      </Box>
     </Box>
   );
 };
