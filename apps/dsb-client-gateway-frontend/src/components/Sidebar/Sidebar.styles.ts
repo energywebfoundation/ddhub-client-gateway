f7ead513c963740679aacc791cd8a5bc20e5a0e1
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2022-08-30 17:02:40 +0800

    feat(dsb-client-gateway-frontend): view for mobile

--- apps/dsb-client-gateway-frontend/src/components/Sidebar/Sidebar.styles.ts
+++ apps/dsb-client-gateway-frontend/src/components/Sidebar/Sidebar.styles.ts
@@ -7,36 +7,11 @@ export const useStyles = makeStyles()((theme) => ({
     display: 'flex',
   },
   drawer: {
-    [theme.breakpoints.up('md')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    backgroundColor: 'unset',
-    boxShadow: 'unset',
-    padding: '5px 0',
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
+    width: drawerWidth,
+    flexShrink: 0,
   },
   drawerPaper: {
     width: drawerWidth,
     padding: '15px 14px 15px',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  closeMenuButton: {
-    marginRight: 'auto',
-    marginLeft: 0,
-  },
-  toolbar: {
-    paddingLeft: 22,
   },
 }));
