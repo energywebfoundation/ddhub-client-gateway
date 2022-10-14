a259a50b2099364a24e77514ea6ca2b7a31d7566
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2022-10-14 13:15:05 +0800

    feat(dsb-client-gateway-frontend): add client subscriptions

---
+++ apps/dsb-client-gateway-frontend/src/pages/client-ids/index.tsx
@@ -0,0 +1,9 @@
+import { ClientIdList } from '@ddhub-client-gateway-frontend/ui/client-ids';
+
+export default function ClientIds() {
+  return (
+    <main>
+      <ClientIdList />
+    </main>
+  );
+}
