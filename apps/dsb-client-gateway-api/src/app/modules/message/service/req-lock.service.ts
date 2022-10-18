aba977bf2872a1beaf9ab8bf45cc1a424cf6375e
Krzysztof Szostak <christopher.szostak@energyweb.org>
2022-10-18 09:37:05 +0200

    fix(dsb-client-gateway-api): clear lock on catch, add logs

--- apps/dsb-client-gateway-api/src/app/modules/message/service/req-lock.service.ts
+++ apps/dsb-client-gateway-api/src/app/modules/message/service/req-lock.service.ts
@@ -1,12 +1,20 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { ReqLockWrapperRepository } from '@dsb-client-gateway/dsb-client-gateway-storage';
+import {
+  ReqLockEntity,
+  ReqLockWrapperRepository,
+} from '@dsb-client-gateway/dsb-client-gateway-storage';
 import { ReqLockExistsException } from '../exceptions/req-lock-exists.exception';
+import moment from 'moment';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ReqLockService {
   protected readonly logger = new Logger(ReqLockService.name);
 
-  constructor(protected readonly reqLockWrapper: ReqLockWrapperRepository) {}
+  constructor(
+    protected readonly reqLockWrapper: ReqLockWrapperRepository,
+    protected readonly configService: ConfigService
+  ) {}
 
   public async clearLock(clientId: string, fqcn: string): Promise<void> {
     await this.reqLockWrapper.repository.delete({
@@ -19,6 +27,41 @@ export class ReqLockService {
 
   public async attemptLock(clientId: string, fqcn: string): Promise<void> {
     try {
+      const existingLock: ReqLockEntity | undefined =
+        await this.reqLockWrapper.repository.findOne({
+          where: {
+            clientId,
+            fqcn,
+          },
+        });
+
+      if (!existingLock) {
+        await this.reqLockWrapper.repository.insert({
+          fqcn,
+          clientId,
+        });
+
+        this.logger.log(`lock with ${fqcn}:${clientId} saved`);
+
+        return;
+      }
+
+      if (
+        moment(existingLock.updatedDate)
+          .add(this.configService.get<number>('REQ_LOCK_TIMEOUT'), 'seconds')
+          .isSameOrBefore(moment())
+      ) {
+        this.logger.log(`outdated lock`, {
+          fqcn,
+          clientId,
+        });
+
+        await this.reqLockWrapper.repository.delete({
+          fqcn,
+          clientId,
+        });
+      }
+
       await this.reqLockWrapper.repository.insert({
         fqcn,
         clientId,
