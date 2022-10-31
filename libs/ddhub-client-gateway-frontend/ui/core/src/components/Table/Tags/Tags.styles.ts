e2793d95a7321ef57a426c54a66092696dd88bf3
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2022-10-31 12:32:22 +0800

    fix(ui-core): show hide tags

--- libs/ddhub-client-gateway-frontend/ui/core/src/components/Table/Tags/Tags.styles.ts
+++ libs/ddhub-client-gateway-frontend/ui/core/src/components/Table/Tags/Tags.styles.ts
@@ -1,5 +1,5 @@
 import { makeStyles } from 'tss-react/mui';
-import { alpha } from '@mui/material/styles';
+import { alpha, darken } from '@mui/material/styles';
 
 export const useStyles = makeStyles()((theme) => ({
   chip: {
@@ -14,5 +14,17 @@ export const useStyles = makeStyles()((theme) => ({
     letterSpacing: '0.4px',
     color: theme.palette.primary.main,
     fontFamily: theme.typography.body2.fontFamily,
+  },
+  chipMore: {
+    background: theme.palette.primary.main,
+    height: 20,
+    borderRadius: 21,
+    '&:hover': {
+      backgroundColor: darken(theme.palette.primary.main, 0.2),
+    },
+  },
+  chipLabelWhite: {
+    color: theme.palette.common.white,
+    fontWeight: 600,
   },
 }));
