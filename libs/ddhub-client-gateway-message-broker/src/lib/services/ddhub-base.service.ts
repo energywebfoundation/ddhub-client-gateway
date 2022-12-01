3c7668538e517803fd86d6473135c5e143d45f91
azam.ismail <azam.ismail@energyweb.org>
2022-12-01 15:35:27 +0800

    feat(dsb-client-gateway-api): add new attribute for error msg

--- libs/ddhub-client-gateway-message-broker/src/lib/services/ddhub-base.service.ts
+++ libs/ddhub-client-gateway-message-broker/src/lib/services/ddhub-base.service.ts
@@ -68,6 +68,7 @@ export abstract class DdhubBaseService {
         e.message,
         DsbClientGatewayErrors.MB_UNKNOWN,
         null,
+        null,
         null
       );
     }
@@ -105,6 +106,7 @@ export abstract class DdhubBaseService {
         e.message,
         DsbClientGatewayErrors.MB_ERROR,
         e.response.data.returnCode,
+        e.response.data.returnMessage,
         e.request.path
       );
     }
@@ -138,6 +140,7 @@ export abstract class DdhubBaseService {
         e.message,
         DsbClientGatewayErrors.MB_ERROR,
         e.response.data.returnCode,
+        e.response.data.returnMessage,
         e.request.path
       );
     }
@@ -151,9 +154,10 @@ export abstract class DdhubBaseService {
     }
 
     throw new MessageBrokerException(
-      `${e.message} -> ${e.response.data.returnMessage}`,
+      e.message,
       DsbClientGatewayErrors.MB_ERROR,
       e.response.data.returnCode,
+      e.response.data.returnMessage,
       e.request.path
     );
   }
