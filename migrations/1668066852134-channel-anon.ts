e79c7fbbf6f129dc69c33175ff330ce9f6748d81
Kim Honoridez <kim.energyweb@gmail.com>
2023-02-16 10:40:12 +0800

    fix(ddhub-client-gateway-storage): change useAnonymousExtChannel default value to false

--- migrations/1668066852134-channel-anon.ts
+++ migrations/1668066852134-channel-anon.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class channelAnon1668066852134 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        ALTER TABLE public.channels ADD COLUMN IF NOT EXISTS "useAnonymousExtChannel" boolean DEFAULT true NOT NULL;
+        ALTER TABLE public.channels ADD COLUMN IF NOT EXISTS "useAnonymousExtChannel" boolean DEFAULT false NOT NULL;
     `);
   }
 
