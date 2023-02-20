e695133dbefdfe4673f6ee5ee864f3f595ecc4f4
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2023-02-20 12:00:49 +0800

    fix(dsb-client-gateway-api): fix next key

--- libs/ddhub-client-gateway-association-keys/src/lib/service/association-keys.service.ts
+++ libs/ddhub-client-gateway-association-keys/src/lib/service/association-keys.service.ts
@@ -68,7 +68,7 @@ export class AssociationKeysService implements OnApplicationBootstrap {
 
     return {
       current: currentKey,
-      next: nextKey,
+      next: nextKey ?? currentKey,
     };
   }
 
