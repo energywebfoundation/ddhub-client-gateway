78f9baf1ec213bf528d60a7ab7cf2ec9662f3b7c
Vicken Liu <vicken.liu@gmail.com>
2023-02-24 14:40:05 +1300

    chore(devops): fix lint

--- apps/dsb-client-gateway-api/src/main.ts
+++ apps/dsb-client-gateway-api/src/main.ts
@@ -23,19 +23,14 @@ dotenv.config({
 });
 
 const generateSchema = async (document) => {
-  if (!process.env.GENERATE_SWAGGER) {
-    return;
-  }
-
   if (!document.components.schemas) {
     document.components.schemas = {};
   }
 
-  const docsPath =
-    process.env.SWAGGER_SCHEMA_PATH ||
-    './libs/dsb-client-gateway-api-client/schema.yaml';
-
-  fs.writeFileSync(docsPath, Yaml.stringify(document));
+  fs.writeFileSync(
+    './libs/dsb-client-gateway-api-client/schema.yaml',
+    Yaml.stringify(document)
+  );
 };
 
 async function bootstrap() {
