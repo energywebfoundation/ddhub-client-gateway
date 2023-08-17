59251633e199c618d5516835131e48aa66da7734
Krzysztof Szostak <christopher.szostak@energyweb.org>
2023-08-17 10:31:29 +0200

    feat(dsb-client-gateway-api): add migration for messages

---
+++ migrations/1692260242937-messages.ts
@@ -0,0 +1,74 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class messages1692260242937 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`
+        ALTER TABLE topic
+          ADD CONSTRAINT "UC_topics_id_version" UNIQUE (id, version);
+
+CREATE TABLE received_messages (
+    messageId VARCHAR(255) PRIMARY KEY,
+    initiatingMessageId VARCHAR(255),
+    clientGatewayMessageId VARCHAR(255) NOT NULL,
+    topicId VARCHAR(255),
+    topicVersion VARCHAR(255),
+    transactionId VARCHAR(255),
+    signature VARCHAR(255),
+    senderDid VARCHAR(255),
+    payloadEncryption BOOLEAN DEFAULT FALSE,
+    payload VARCHAR(255),
+    timestampNanos TIMESTAMP,
+    isFile BOOLEAN DEFAULT FALSE,
+    createdDate DATE DEFAULT CURRENT_DATE,
+    updatedDate DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (topicId, topicVersion) REFERENCES topic(id, version) ON DELETE SET NULL
+);
+
+CREATE TABLE received_messages_mapping (
+    fqcn VARCHAR(255) PRIMARY KEY,
+    messageId VARCHAR(255) REFERENCES received_messages(messageId)
+);
+
+CREATE TABLE received_messages_read_status (
+    messageId VARCHAR(255) REFERENCES received_messages(messageId),
+    recipientUser VARCHAR(255),
+    PRIMARY KEY (messageId, recipientUser)
+);
+
+CREATE TABLE sent_messages (
+    initiatingMessageId VARCHAR(255),
+    clientGatewayMessageId VARCHAR(255) NOT NULL,
+    topicId VARCHAR(255),
+    topicVersion VARCHAR(255),
+    transactionId VARCHAR(255),
+    signature VARCHAR(255),
+    payloadEncryption BOOLEAN DEFAULT FALSE,
+    payload VARCHAR(255),
+    timestampNanos TIMESTAMP,
+    isFile BOOLEAN DEFAULT FALSE,
+    totalRecipients NUMERIC,
+    totalSent NUMERIC,
+    totalFailed NUMERIC,
+    createdDate DATE DEFAULT CURRENT_DATE,
+    updatedDate DATE DEFAULT CURRENT_DATE,
+    PRIMARY KEY (clientGatewayMessageId),
+    FOREIGN KEY (topicId, topicVersion) REFERENCES topic(id, version) ON DELETE SET NULL
+);
+
+CREATE TABLE sent_messages_recipients (
+    clientGatewayMessageId VARCHAR(255) REFERENCES sent_messages(clientGatewayMessageId),
+    messageId VARCHAR(255) PRIMARY KEY,
+    recipientDid VARCHAR(255),
+    status VARCHAR(255),
+    statusCode NUMERIC,
+    createdDate DATE DEFAULT CURRENT_DATE,
+    updatedDate DATE DEFAULT CURRENT_DATE
+);
+      `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
