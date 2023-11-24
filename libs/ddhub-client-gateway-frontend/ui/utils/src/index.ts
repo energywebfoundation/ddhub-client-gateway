efe6cf67d2b2cf16a52e3ecd6aeb6504a1e65676
nik-ewf <nikolaus.wust@energyweb.org>
2023-11-24 12:58:44 +0800

    chore(ddhub-client-gateway-ui-messaging): add missing util export

--- libs/ddhub-client-gateway-frontend/ui/utils/src/index.ts
+++ libs/ddhub-client-gateway-frontend/ui/utils/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/downloadJson';
 export * from './lib/downloadFile';
 export * from './lib/did-format-minifier';
 export * from './lib/capitalizeFirstLetter';
+export * from './lib/generateUuid';
