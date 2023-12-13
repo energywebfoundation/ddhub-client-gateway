ec9db83072deabb9a81bd3b1ce40adb484c3e07a
Vicken Liu <vicken.liu@gmail.com>
2023-12-14 12:25:59 +1300

    chore(devops): run build for affected apps only

--- apps/dsb-client-gateway-frontend/src/pages/applications/index.tsx
+++ apps/dsb-client-gateway-frontend/src/pages/applications/index.tsx
@@ -3,6 +3,5 @@ import { UserRole } from '@ddhub-client-gateway-frontend/ui/login';
 import { useRouteGuard } from '../../utils/RouteGuard.effects';
 
 export default function ListApplications() {
-  console.log('trigger change');
   return useRouteGuard(Applications, UserRole.ADMIN);
 }
