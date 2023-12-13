fbe858bd01e0115ad08bf4cc482b1e8024ca436b
Vicken Liu <vicken.liu@gmail.com>
2023-12-14 11:19:57 +1300

    chore(devops): test frontend change

--- apps/dsb-client-gateway-frontend/src/pages/applications/index.tsx
+++ apps/dsb-client-gateway-frontend/src/pages/applications/index.tsx
@@ -3,5 +3,6 @@ import { UserRole } from '@ddhub-client-gateway-frontend/ui/login';
 import { useRouteGuard } from '../../utils/RouteGuard.effects';
 
 export default function ListApplications() {
+  console.log('trigger change');
   return useRouteGuard(Applications, UserRole.ADMIN);
 }
