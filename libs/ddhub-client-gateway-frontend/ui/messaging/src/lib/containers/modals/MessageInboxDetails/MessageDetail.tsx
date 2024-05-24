1a80bbd6f18b6000ae24e5777f2c18cbdde88c4a
nik-ewf <nikolaus.wust@energyweb.org>
2024-05-24 13:05:34 +0800

    fix(dsb-client-gateway-frontend): regex check for potential iso date

--- libs/ddhub-client-gateway-frontend/ui/messaging/src/lib/containers/modals/MessageInboxDetails/MessageDetail.tsx
+++ libs/ddhub-client-gateway-frontend/ui/messaging/src/lib/containers/modals/MessageInboxDetails/MessageDetail.tsx
@@ -16,14 +16,24 @@ interface MessageDetailProps {
   };
 }
 
+// eslint-disable no-useless-escape
+const isISODateString = (value: string) =>
+  /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/.test(
+    value
+  );
+// eslint-enable no-useless-escape
+
 export const MessageDetail: FC<MessageDetailProps> = ({
   field,
 }: MessageDetailProps) => {
   const { classes } = useStyles();
   let valueFieldText = field.value;
-  const valueAsDateTime = DateTime.fromISO(valueFieldText);
-  if (valueAsDateTime.isValid) {
-    valueFieldText = valueAsDateTime.toFormat('dd/MM/yyyy HH:mm:ss');
+
+  if (isISODateString(valueFieldText)) {
+    const valueAsDateTime = DateTime.fromFormat(valueFieldText, '');
+    if (valueAsDateTime.isValid) {
+      valueFieldText = valueAsDateTime.toFormat('dd/MM/yyyy HH:mm:ss');
+    }
   }
 
   const parsedArrayItem: any[] = [];
