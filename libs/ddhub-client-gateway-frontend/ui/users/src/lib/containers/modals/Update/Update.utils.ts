b1e62dd1f2f91f888300d7fd0def8f69dde8a2ca
Justyna Łempicka-Wojno <justyna.lempicka-wojno@energyweb.org>
2025-08-28 21:20:03 +0200

    chore: update vault-prepare.sh and update password requirements

--- libs/ddhub-client-gateway-frontend/ui/users/src/lib/containers/modals/Update/Update.utils.ts
+++ libs/ddhub-client-gateway-frontend/ui/users/src/lib/containers/modals/Update/Update.utils.ts
@@ -24,19 +24,18 @@ export const getChangePasswordSchema = () =>
       .oneOf([yup.ref('newPassword')], 'Passwords do not match'),
   });
 
-// commented-out because current passwords from vault-prepare.sh are not meeting the requirements
 export const getVerifyPasswordSchema = () =>
   yup.object().shape({
     [VERIFY_PASSWORD_AVAILABLE_FIELDS.password]: yup
       .string()
-      .required('Password is required'),
-    // .min(8, 'Password must be at least 8 characters')
-    // .max(256, 'Password cannot exceed 256 characters')
-    // .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    // .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-    // .matches(/[0-9]/, 'Password must contain at least one number')
-    // .matches(
-    //   /[^A-Za-z0-9]/,
-    //   'Password must contain at least one special character'
-    // ),
+      .required('Password is required')
+      .min(8, 'Password must be at least 8 characters')
+      .max(256, 'Password cannot exceed 256 characters')
+      .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+      .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+      .matches(/[0-9]/, 'Password must contain at least one number')
+      .matches(
+        /[^A-Za-z0-9]/,
+        'Password must contain at least one special character'
+      ),
   });
