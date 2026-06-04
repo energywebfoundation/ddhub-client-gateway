c41de88f984f920c5d25035e787d1da5a728a2e4
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2026-06-04 16:43:07 +0800

    feat(ddhub-client-gateway-ui-api-hooks): api keys api hooks

---
+++ libs/ddhub-client-gateway-frontend/ui/api-hooks/src/lib/apiKeys/updateApiKey.ts
@@ -0,0 +1,49 @@
+import { useCustomAlert } from '@ddhub-client-gateway-frontend/ui/core';
+import {
+  UserApiKeyControllerUpdateApiKeyBody,
+  useUserApiKeyControllerUpdateApiKey,
+} from '@dsb-client-gateway/dsb-client-gateway-api-client';
+
+export const useUpdateApiKey = () => {
+  const Swal = useCustomAlert();
+
+  const { mutate, isLoading } = useUserApiKeyControllerUpdateApiKey();
+
+  const updateError = (err: any, name: string) => {
+    console.error(err);
+    console.log(err.response);
+
+    Swal.warning({
+      title: `${name} update failed`,
+      text: `Unable to update ${name}. Please try again.`,
+      showCancelButton: false,
+      confirmButtonText: 'Dismiss',
+    });
+  };
+
+  const updateApiKeyHandler = (
+    apiKey: string,
+    values: UserApiKeyControllerUpdateApiKeyBody,
+    onSuccess: () => void
+  ) => {
+    const { name, daysValid } = values;
+    mutate(
+      {
+        apiKey,
+        data: {
+          name,
+          daysValid,
+        },
+      },
+      {
+        onSuccess,
+        onError: (err) => updateError(err, name),
+      }
+    );
+  };
+
+  return {
+    updateApiKeyHandler,
+    isLoading,
+  };
+};
