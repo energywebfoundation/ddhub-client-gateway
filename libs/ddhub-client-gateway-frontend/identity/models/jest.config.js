e23b7f35c4b789630ef7c29e7e551626a6bbc155
nadia-adila <105151876+nadia-adila@users.noreply.github.com>
2026-06-11 16:46:03 +0800

    fix: jest test

--- libs/ddhub-client-gateway-frontend/identity/models/jest.config.js
+++ libs/ddhub-client-gateway-frontend/identity/models/jest.config.js
@@ -13,4 +13,5 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory:
     '../../../../coverage/libs/ddhub-client-gateway-frontend/identity/models',
+  passWithNoTests: true,
 };
